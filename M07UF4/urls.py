6142a7185a1978dc5d3c407450733c4c4c6ef72a
lcastienc <lcastill@jaumebalmes.net>
2023-04-20 20:32:20 +0200

    modificaciones act16

--- M07UF4/urls.py
+++ M07UF4/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.index,name='index_one'),
     path('teachers', views.teachers,name='teachers'),
     path('students', views.students, name='students'),
-    # path('index2', views.index, name='index'),
+    path('student/<str:pk>/', views.student, name='student'),
+    path('teacher/<str:pk>/', views.teacher, name='teacher')
+
+    # path('index2', views.index, name='index'),3
 ]
 
--- M07UF4/views.py
+++ M07UF4/views.py
@@ -48,70 +48,66 @@ teachers= [
         "age": "44"
     }
 ]
+students = [
+    {
+        "id": "4",
+        "name": "Kevin",
+        "surname": "Villegas",
+        "age": "23"
+    },
+    {
+        "id": "5",
+        "name": "Raul",
+        "surname": "Rufo",
+        "age": "20"
+    },
+    {
+        "id": "6",
+        "name": "Raul",
+        "surname": "Vaquerizo",
+        "age": "20"
+    },
+    {
+        "id": "7",
+        "name": "Joan",
+        "surname": "Gimenez",
+        "age": "25"
+    },
+    {
+        "id": "8",
+        "name": "Pepito",
+        "surname": "Pancho",
+        "age": "33"
+    },
+    {
+        "id": "9",
+        "name": "Sancho",
+        "surname": "Panza",
+        "age": "18"
+    }
+]
 
 
 def teachers(request):
-    teachers = [
-        {
-            "id": "1",
-            "name": "Luis",
-            "surname": "Castillo",
-            "age": "22"
-        },
-        {
-            "id": "2",
-            "name": "Leonardo",
-            "surname": "Ricaurte",
-            "age": "33"
-        },
-        {
-            "id": "3",
-            "name": "Andres",
-            "surname": "Carles",
-            "age": "44"
-        }
-    ]
     context = {'ts':teachers}
     return render(request, 'teachers.html',context)
 
 def students(request):
-    students = [
-        {
-            "id": "4",
-            "name": "Kevin",
-            "surname": "Villegas",
-            "age": "23"
-        },
-        {
-            "id": "5",
-            "name": "Raul",
-            "surname": "Rufo",
-            "age": "20"
-        },
-        {
-            "id": "6",
-            "name": "Raul",
-            "surname": "Vaquerizo",
-            "age": "20"
-        },
-        {
-            "id": "7",
-            "name": "Joan",
-            "surname": "Gimenez",
-            "age": "25"
-        },
-        {
-            "id": "8",
-            "name": "Pepito",
-            "surname": "Pancho",
-            "age": "33"
-        },
-        {
-            "id": "9",
-            "name": "Sancho",
-            "surname": "Panza",
-            "age": "18"
-        }
-    ]
     context = {'stds':students}
     return render(request,'students.html',context)
+
+def student(request, pk):
+    estudiante = None
+    for i in students:
+        if i['id'] == int(pk):
+            estudiante = i
+    return  render(request, 'student.html', {'std':estudiante})
+def teacher(request, pk):
+    profe = None
+    for i in teachers:
+        if i['id'] == int(pk):
+            profe = i
+    return render(request, 'teacher.html', {'t':profe})
+
+
+
